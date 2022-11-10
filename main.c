e59391ce3364f6b1eea832520f5b1a24f44fe4ca
tamimmuheb <99010155+tamimmuheb@users.noreply.github.com>
2022-11-10 14:29:05 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define PI 3.1416
+const double pi = 22.0/7.0;
 
 int main()
 {
-
-float rad,area, perm;
+    float rad,area, perm;
 
     printf("Enter radius of circle: ");
     scanf("%f",&rad);
 
-    area=PI*rad*rad;
-    perm=2*PI*rad;
+    area= pi*rad*rad;
+    perm=2*pi*rad;
 
     printf("Area of circle: %f \nPerimeter of circle: %f\n",area,perm);
     return 0;
